516ac66aff85e33ae3e9a549aa8b3aa4942a30ee
Miriam Peralta <miiriampc@gmail.com>
2017-04-28 12:51:55 -0500

    valida captcha

---
+++ assets/js/app.js
@@ -0,0 +1,40 @@
+window.addEventListener('load',function(){
+  var inputCaptcha=document.getElementById("captcha");
+  var inputTextCaptcha=document.getElementById('copyCaptcha');
+  generaCaptcha(inputCaptcha);
+
+  document.getElementById('check').addEventListener('click',function(){
+    console.log("aaki");
+    if(verifyCaptcha(inputCaptcha,inputTextCaptcha)){
+      alert("Código valido");
+    }else {
+      alert("El código no es correcto");
+    }
+  });
+
+  document.getElementById('reboot').addEventListener('click',function(){
+    generaCaptcha(inputCaptcha);
+    document.getElementById('copyCaptcha').value="";
+  });
+
+});
+function generaCaptcha(input){
+  var regex=/[A-Za-z0-9]{1}/;
+  // var caracter=new RegExp(regex);
+  var caracter = new Array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+
+  for (var i=0;i<6;i++){
+    var c1 = caracter[Math.floor(Math.random() * caracter.length)];
+    var c2 = caracter[Math.floor(Math.random() * caracter.length)];
+    var c3 = caracter[Math.floor(Math.random() * caracter.length)];
+    var c4 = caracter[Math.floor(Math.random() * caracter.length)];
+    var c5 = caracter[Math.floor(Math.random() * caracter.length)];
+    var c6 = caracter[Math.floor(Math.random() * caracter.length)];
+    var c7 = caracter[Math.floor(Math.random() * caracter.length)];
+   }
+   input.value = c1 + c2 + c3 + c4 + c5 + c6 + c7;
+}
+
+function verifyCaptcha(captcha,captchaCopy){
+  return (captcha.value!=captchaCopy.value) ? false:true;
+}
